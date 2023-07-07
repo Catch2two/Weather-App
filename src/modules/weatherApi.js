931b67a40e7f236621584c207fea203a5cf0eebc
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-07 15:11:39 -0400

    Attempting color gradient based on temperature.

--- src/modules/weatherApi.js
+++ src/modules/weatherApi.js
@@ -30,16 +30,49 @@ async function fetchWeather() {
         const cityElement = document.querySelector(".weatherCity")
         cityElement.innerHTML = city;
         const latElement = document.querySelector(".weatherLatitude")
-        latElement.innerHTML = latitude;
+        latElement.innerHTML = `Lat: ${latitude}`;
         const lonElement = document.querySelector(".weatherLongitude")
-        lonElement.innerHTML = longitude;
+        lonElement.innerHTML = `Lon: ${longitude}`;
         // Temperature
         const temperatureELement_F = document.querySelector(".weatherUnitF")
         temperatureELement_F.innerHTML = `${temperatureF}°F`;
         const temperatureELement_C = document.querySelector(".weatherUnitC")
         temperatureELement_C.innerHTML = `${temperatureC}°C`;
         const feelsLikeElement_F = document.querySelector(".weatherFeelsLikeF")
-        feelsLikeElement_F.innerHTML = feelsLikeF;
+        feelsLikeElement_F.innerHTML = `Feels Like: ${feelsLikeF}°F`;
+        const feelsLikeElement_C = document.querySelector(".weatherFeelsLikeC")
+        feelsLikeElement_C.innerHTML = `Feels Like: ${feelsLikeC}°C`;
+        // If the temperature is above 90f or 32c, make it red
+        if (temperatureF > 90) {
+            temperatureELement_F.style.color = "red";
+        } else {
+            // Calculate the gradient color based on the temperature
+            const gradientColor = `linear-gradient(to right, red, rgba(255, 200, 0, 1 - (temperatureF / 32))`;
+            temperatureELement_F.style.color = gradientColor;
+        }
+
+        if (feelsLikeF > 90) {
+            feelsLikeElement_F.style.color = "red";
+        } else {
+            const gradientColor = `linear-gradient(to right, red, rgba(255, 200, 0, 1 - (feelsLikeF / 32))`;
+            feelsLikeElement_F.style.color = gradientColor;
+        }
+
+        if (temperatureC > 32.2) {
+            temperatureELement_C.style.color = "red";
+        } else {
+            const gradientColor = `linear-gradient(to right, red, rgba(255, 200, 0, 1 - (temperatureC / 32.2))`;
+            temperatureELement_C.style.color = gradientColor;
+        }
+
+        if (feelsLikeC > 32.2) {
+            feelsLikeElement_C.style.color = "red";
+        } else {
+            const gradientColor = `linear-gradient(to right, red, rgba(255, 200, 0, 1 - (feelsLikeC / 32.2))`;
+            feelsLikeElement_C.style.color = gradientColor;
+        }
+        // Humidity
+     
         // Weather Description
 
 
