aa135fd8554812207c7c5f50a5aff23732a2fd3e
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-11 15:52:23 -0400

    Ongoing: Search Function

--- src/modules/weatherForecast.js
+++ src/modules/weatherForecast.js
@@ -2,7 +2,7 @@ const apiKey = "6fe5d9b89c10408d90d143901232806"
 
 async function fetchForecast() {
     const city = "06021";
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=6fe5d9b89c10408d90d143901232806&q=${city}&days=5&aqi=yes&alerts=yes`;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=6fe5d9b89c10408d90d143901232806&q=${city}&days=7&aqi=yes&alerts=yes`;
     const response = await fetch(url);
     // Call API
     if (response.status === 200) {
@@ -12,17 +12,19 @@ async function fetchForecast() {
     // DOM Selectors
   
       const forecastElement = document.querySelector(".weatherForecast");
-    // Forecast div
+      const today = new Date();
+
       for (const day of forecastday) {
         const date = new Date(day.date);
-
-  
-        forecastElement.innerHTML += `
-          <h2>${date}:</h2>
-          <p>High: ${day.high}°F</p>
-          <p>Low: ${day.low}°F</p>
-          <p>Conditions: ${day.conditions}</p>
-        `;
+        // Start day after Today.
+        if (date > today) {
+          forecastElement.innerHTML += `
+            <h2>${date}:</h2>
+            <p>High: ${day.high}°F</p>
+            <p>Low: ${day.low}°F</p>
+            <p>Conditions: ${day.conditions}</p>
+          `;
+        }
       }
       return data;
     } else {
