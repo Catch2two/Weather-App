1ec87a898a7addcaa9cf73adfd26b774b4340c67
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-12 14:13:06 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import { fetchForecast } from "./modules/weatherForecast";
 import { handleSearch } from "./modules/locationSearch";
 
 const weatherData = await fetchWeather();
+const forecastData = await fetchForecast();
 
 if (weatherData) {
     console.log(weatherData);
   } else {
     console.log("Error fetching weather data");
-  }
+  };
 
-  const forecastData = await fetchForecast();
-
-  if (forecastData) {
+if (forecastData) {
     console.log(forecastData);
   } else {
     console.log("Error fetching forecast data")
   }
-console.log("src JS's loaded.")
