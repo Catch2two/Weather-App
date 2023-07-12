85417adc1658f29b057678b288aba71d7ad6c616
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-12 12:48:31 -0400

    Forecast(Free api) created

--- src/modules/weatherForecast.js
+++ src/modules/weatherForecast.js
@@ -2,7 +2,7 @@ const apiKey = "6fe5d9b89c10408d90d143901232806"
 
 async function fetchForecast() {
     const city = "06021";
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=6fe5d9b89c10408d90d143901232806&q=${city}&days=7&aqi=yes&alerts=yes`;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=6fe5d9b89c10408d90d143901232806&q=${city}&days=4&aqi=yes&alerts=yes`;
     const response = await fetch(url);
     // Call API
     if (response.status === 200) {
@@ -11,24 +11,26 @@ async function fetchForecast() {
   
     // DOM Selectors
   
-      const forecastElement = document.querySelector(".weatherForecast");
-      const today = new Date();
-
-      for (const day of forecastday) {
-        const date = new Date(day.date);
-        // Start day after Today.
-        if (date > today) {
-          forecastElement.innerHTML += `
-            <h2>${date}:</h2>
-            <p>High: ${day.high}°F</p>
-            <p>Low: ${day.low}°F</p>
-            <p>Conditions: ${day.conditions}</p>
-          `;
-        }
+    const forecastElement = document.querySelector(".weatherForecast");
+    const today = new Date();
+    
+    for (let i = 0; i < 3; i++) {
+      const date = new Date(forecastday[i].date);
+      // Start day after Today.
+      if (date > today) {
+        const dayOfWeek = date.getDay();
+        const dayOfMonth = date.getDate();
+        const dayOfWeekString = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][dayOfWeek];
+        
+        forecastElement.innerHTML += `
+          <h2>${dayOfWeekString} ${dayOfMonth}:</h2>
+          <p>High: ${forecastday[i].day.maxtemp_f}°F</p>
+          <p>Low: ${forecastday[i].day.mintemp_f}°F</p>
+          <p>Conditions: ${forecastday[i].day.condition.text}</p>
+        `;
       }
-      return data;
-    } else {
-      return null;
+    }
+      return data.forecast.forecastday;
     }
   };
   console.log("Forecast Loaded")
