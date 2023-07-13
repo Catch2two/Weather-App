96bafca280d55cf9a81dd57cfe46aef8cfce08c0
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-13 12:14:55 -0400

    Trial and Error (I know...)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import { fetchWeather } from "./modules/weatherCurrent";
 import { fetchForecast } from "./modules/weatherForecast";
 
-import fetchWeather from "./fetchWeather";
-import forecast from "./forecast";
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const city = document.querySelector("#city").value;
-  const weatherData = fetchWeather(city);
-  const forecastData = forecast(city);
-});
+fetchWeather()
+fetchForecast()
