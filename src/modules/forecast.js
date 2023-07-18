655fda57da7eb866ec88a0b3b11c997bc83bf943
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-18 13:55:13 -0400

    removed forecastDOM

---
+++ src/modules/forecast.js
@@ -0,0 +1,39 @@
+import api from "./api.js";
+const url = api();
+const forecastElement = document.querySelector(".weatherForecast");
+const today = new Date();
+
+async function fetchForecast() {
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const data = await response.json();
+      const forecastday = data.forecast.forecastday;
+  
+      // Cycle through the Forecast Array
+      for (let i = 0; i < 3; i++) {
+        const date = new Date(forecastday[i].date);
+  
+        // Start day after Today.
+        if (date > today && date <= (new Date()).setDate(today.getDate() + 3)) {
+          const dayOfWeek = date.getDay();
+          const dayOfMonth = date.getDate();
+          const dayOfWeekString = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][dayOfWeek];
+  
+          forecastElement.innerHTML += `
+            <div>
+              <h2>${dayOfWeekString} ${dayOfMonth}</h2>
+              <h4> in ${data.location.name} </h4>
+              <ul>
+                <li>High: ${forecastday[i].day.maxtemp_f}°F</li>
+                <li>Low: ${forecastday[i].day.mintemp_f}°F</li>
+                <li>Conditions: ${forecastday[i].day.condition.text}</li>
+              </ul>
+            </div>`;
+        }
+      }
+    } else {
+      alert("Something went wrong.");
+    }
+  };
+
+  export {fetchForecast}
