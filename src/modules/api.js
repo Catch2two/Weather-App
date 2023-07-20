a67d96196d6aec0c729b76eb08965bf423d64149
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-20 12:31:36 -0400

    Submit is such a pain.

--- src/modules/api.js
+++ src/modules/api.js
@@ -6,11 +6,13 @@ return url;
 }
 
 const form = document.querySelector("form");
+const cityInput = document.querySelector("#cityInput")
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const newInput = document.querySelector("input[name='input']").value;
-  city = newInput;
-  console.log(api(city));
+  const city = cityInput.value;
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=6fe5d9b89c10408d90d143901232806&q=${city}&days=3&aqi=yes&alerts=yes`;
+  console.log(url)
 });
 
 export default api;
