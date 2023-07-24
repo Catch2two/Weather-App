948b073e729d86d9f31befc2a38b73432cad34a4
Josh <118862870+Catch2two@users.noreply.github.com>
2023-07-24 13:32:52 -0400

    Reverted back to old fetchForecast()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { fetchWeather} from "./modules/currentLOGIC";
+import { fetchForecast } from "./modules/forecast";
 // import { getWeatherMap } from "./modules/map";
 fetchWeather()
+fetchForecast()
